47945269283ff20f686889d80ae246874e2962e8
Hu Yao-Chieh <yhuag@connect.ust.hk>
2019-01-11 10:41:36 +0800

    Updates

---
+++ precache-manifest.b0d1b5cda8ca275d5d8c3ac1d20350f0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "969b785a54e9bd9b0735",
+    "url": "./static/js/main.969b785a.chunk.js"
+  },
+  {
+    "revision": "8686f2d107e8d4e42a85",
+    "url": "./static/js/1.8686f2d1.chunk.js"
+  },
+  {
+    "revision": "969b785a54e9bd9b0735",
+    "url": "./static/css/main.4dc93d97.chunk.css"
+  },
+  {
+    "revision": "ef61ef0b580775f80be274c2e8cf59e1",
+    "url": "./index.html"
+  }
+];
